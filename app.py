bc2f17a9caa84ec7594e77ce4d4457668a148928
Manjunath CT <158074645+manjunathct2005@users.noreply.github.com>
2025-07-22 18:06:14 +0530

    app.py

--- app.py
+++ app.py
@@ -2,18 +2,13 @@
 import streamlit as st
 from llm_backend import get_answer
 
-st.set_page_config(page_title="🌐 Multilingual LLM", layout="centered")
+st.set_page_config(page_title="🌐 Multilingual Question Answering", layout="centered")
 st.title("🌐 Multilingual Question Answering")
-st.markdown("Ask a question in **English**, **Hindi**, **Telugu**, or **Kannada**.")
+st.markdown("Ask a question in **English, Hindi, Telugu, or Kannada**")
 
-# Input box
-query = st.text_input("📝 Enter your question", max_chars=500)
+question = st.text_input("📝 Enter your question", "")
 
-# Process
-if st.button("💬 Get Answer"):
-    if query.strip() == "":
-        st.warning("Please enter a valid question.")
-    else:
-        with st.spinner("Thinking..."):
-            answer = get_answer(query)
-            st.markdown("<span style='color:red'><b>Answer:</b></span><br>---<br>" + answer, unsafe_allow_html=True)
+if question:
+    with st.spinner("Thinking..."):
+        answer = get_answer(question)
+    st.markdown("<br><span style='color:red'><b>Answer:</b></span><br>---<br>" + answer, unsafe_allow_html=True)
